984f274d2f82ad759086bc7a6ececc983a6fb228
Rajangane <pratiksharajangane7@gmail.com>
2021-12-07 12:28:51 +0530

    [Pratiksha] ADD : Finding Integer Maximum Number

---
+++ Generics/Generics/MaximumNum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Given 3 Integers Finding Maximum
+namespace Generics
+{
+    public class MaximumNum
+    {
+        public static int MaximumIntNumber(int firstNum, int secondNum, int thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+            {
+                return firstNum;
+            }
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0)
+            {
+                return secondNum;
+            }
+            if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) > 0)
+            {
+                return thirdNum;
+            }
+            throw new Exception("FirstNumber, SecondNumber and ThirdNumber are same or Maybe 2 Numbers are Same");
+        }
+    }
+}
--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("**************** Welcome To Generics *********************");
+//Console.WriteLine("**************** Welcome To Generics *********************");
+using Generics;
+Console.Write("Select Number:\n1)IntegerMax\n");
+int option = Convert.ToInt32(Console.ReadLine());
+switch (option)
+{
+    case 1:
+        int output = MaximumNum.MaximumIntNumber(10, 20, 30);
+        Console.WriteLine("Maximum Number:" + output);
+        break;
+}
