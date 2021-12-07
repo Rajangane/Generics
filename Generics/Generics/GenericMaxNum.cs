63f8e7d7830cb6bf949dc71ee991b62ce5764017
Rajangane <pratiksharajangane7@gmail.com>
2021-12-07 13:19:18 +0530

    [Pratiksha] ADD : Print Max Number Using Generic

---
+++ Generics/Generics/GenericMaxNum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//extend the max method to also print the max to std out using genric method
+namespace Generics
+{
+    public class GenericMaxNum<T> where T : IComparable
+    {
+        public T[] value;
+        public GenericMaxNum(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[sorted_values.Length - 1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum value is " + max);
+            
+        }
+    }
+}
--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("**************** Welcome To Generics *********************");
 using Generics;
-Console.Write("Select Number:\n1)IntegerMax\n2)FloatMax\n3)StringMax\n4)SortMaxNum");
+Console.Write("Select Number:\n1)IntegerMax\n2)FloatMax\n3)StringMax\n4)SortMaxNum\n5)GenericMaxNum");
 int option = Convert.ToInt32(Console.ReadLine());
 switch (option)
 {
@@ -22,6 +22,19 @@ switch (option)
         SortMaxNum<int> checkInt1 = new SortMaxNum<int>(output3);
         int i = checkInt1.MaxValue(output3);
         Console.WriteLine(i);
+        break;
+    case 5:
+        int[] intArray = { 10, 20, 30, 40, 50 };
+        GenericMaxNum<int> checkInt2 = new GenericMaxNum<int>(intArray);
+        checkInt2.PrintMaxValue();
+
+        float[] floatArray = { 1.1f, 1.2f, 1.3f, 1.4f, 1.5f };
+        GenericMaxNum<float> checkFloat2 = new GenericMaxNum<float>(floatArray);
+        checkFloat2.PrintMaxValue();
+
+        string[] stringArray = { "555", "666", "777", "888", "999" };
+        GenericMaxNum<string> checkString2 = new GenericMaxNum<string>(stringArray);
+        checkString2.PrintMaxValue();
         break;
     default:
         Console.WriteLine("Invalid Choice");
