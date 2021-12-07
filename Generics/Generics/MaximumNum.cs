835341922271d0b6af7e78e9ffdccc1e261b8227
Rajangane <pratiksharajangane7@gmail.com>
2021-12-07 12:36:03 +0530

    [Pratiksha] ADD : Finding Floating Poin Maximum Number

--- Generics/Generics/MaximumNum.cs
+++ Generics/Generics/MaximumNum.cs
@@ -25,5 +25,22 @@ namespace Generics
             }
             throw new Exception("FirstNumber, SecondNumber and ThirdNumber are same or Maybe 2 Numbers are Same");
         }
+        //Float method to find Maximum Number
+        public static float MaxFloatNumber(float firstNum, float secondNum, float thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+            {
+                return firstNum;
+            }
+            if (secondNum.CompareTo(thirdNum) > 0 && secondNum.CompareTo(firstNum) > 0)
+            {
+                return secondNum;
+            }
+            if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) > 0)
+            {
+                return thirdNum;
+            }
+            throw new Exception("FirstNumber, SecondNumber and ThirdNumber are same or Maybe 2 Numbers are Same");
+        }
     }
 }
--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("**************** Welcome To Generics *********************");
 using Generics;
-Console.Write("Select Number:\n1)IntegerMax\n");
+Console.Write("Select Number:\n1)IntegerMax\n2)FloatMax");
 int option = Convert.ToInt32(Console.ReadLine());
 switch (option)
 {
@@ -9,4 +9,9 @@ switch (option)
         int output = MaximumNum.MaximumIntNumber(10, 20, 30);
         Console.WriteLine("Maximum Number:" + output);
         break;
+    case 2:
+        float output1 = MaximumNum.MaxFloatNumber(1.1f, 2.2f, 3.3f);
+        Console.WriteLine("Maximum Number:" + output1);
+        break;
+
 }
